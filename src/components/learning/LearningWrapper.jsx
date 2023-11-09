d9b8d3df8d6db5063f2506f5bf47334a863529af
Andrzej Legutko <andrzejlegutko@gmail.com>
2023-11-09 19:48:17 +0100

    set LearningWrapper, flashcards in api and getFlashcards

--- src/components/learning/LearningWrapper.jsx
+++ src/components/learning/LearningWrapper.jsx
@@ -1,12 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getDeckandFlashcards } from "/src/api/api.js";
+import Flashcard from "../Flashcards/Flashcard.jsx";
 
-function Layout() {
-    return (
-        <div>
-            <h2>Menu</h2>
-            <Outlet />
-        </div>
+function LearningWrapper() {
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState(null);
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    getDeckandFlashcards(deckId, controller.signal).then(
+      ([deckResponse, flashcardsResponse]) => {
+        setDeck(deckResponse);
+        setFlashcards(flashcardsResponse);
+      },
     );
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  if (!deck) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      Learning: {deckId} {deck.name} {deck.description}
+      <Flashcard
+        question={flashcards[0].question}
+        answer={flashcards[0].answer}
+      />
+    </div>
+  );
 }
 
-export default Layout;
+export default LearningWrapper;
